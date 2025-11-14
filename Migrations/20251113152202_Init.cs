71764c24cc95166f2966757e2b50e33da406ce0d
NhatDz <cunhat240907@gmail.com>
2025-11-14 08:04:41 +0700

    Add EF Core migration InitialCreate

---
+++ Migrations/20251113152202_Init.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASM_C_4.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Avatar = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<int>(type: "INTEGER", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 11, nullable: true),
+                    PassWord = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EventName = table.Column<string>(type: "TEXT", nullable: false),
+                    EventAddress = table.Column<string>(type: "TEXT", nullable: true),
+                    Image = table.Column<string>(type: "TEXT", nullable: true),
+                    SubImage = table.Column<string>(type: "TEXT", nullable: true),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    StartEvent = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EventType = table.Column<string>(type: "TEXT", nullable: false),
+                    EventStatus = table.Column<string>(type: "TEXT", nullable: false),
+                    BankNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    BankName = table.Column<string>(type: "TEXT", nullable: true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                    table.ForeignKey(
+                        name: "FK_Events_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SeatingCharts",
+                columns: table => new
+                {
+                    SeatingChartId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PosX = table.Column<int>(type: "INTEGER", nullable: true),
+                    PosY = table.Column<int>(type: "INTEGER", nullable: true),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeatingCharts", x => x.SeatingChartId);
+                    table.ForeignKey(
+                        name: "FK_SeatingCharts_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShowTimes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowTimes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShowTimes_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SeatGroups",
+                columns: table => new
+                {
+                    SeatGroupId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    Rotate = table.Column<string>(type: "TEXT", nullable: true),
+                    PosX = table.Column<int>(type: "INTEGER", nullable: false),
+                    PosY = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cols = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rows = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeatingChartId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeatGroups", x => x.SeatGroupId);
+                    table.ForeignKey(
+                        name: "FK_SeatGroups_SeatingCharts_SeatingChartId",
+                        column: x => x.SeatingChartId,
+                        principalTable: "SeatingCharts",
+                        principalColumn: "SeatingChartId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    PaymentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.PaymentId);
+                    table.ForeignKey(
+                        name: "FK_Payments_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Refunds",
+                columns: table => new
+                {
+                    RefundId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Reason = table.Column<string>(type: "TEXT", nullable: true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Refunds", x => x.RefundId);
+                    table.ForeignKey(
+                        name: "FK_Refunds_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    SeatId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SeatName = table.Column<string>(type: "TEXT", nullable: true),
+                    PosX = table.Column<int>(type: "INTEGER", nullable: false),
+                    PosY = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeatGroupId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.SeatId);
+                    table.ForeignKey(
+                        name: "FK_Seats_SeatGroups_SeatGroupId",
+                        column: x => x.SeatGroupId,
+                        principalTable: "SeatGroups",
+                        principalColumn: "SeatGroupId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShowTimeTicketGroups",
+                columns: table => new
+                {
+                    ShowTimeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeatGroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    MaxTicket = table.Column<int>(type: "INTEGER", nullable: false),
+                    TicketSaleStart = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TicketSaleEnd = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowTimeTicketGroups", x => new { x.ShowTimeId, x.SeatGroupId });
+                    table.ForeignKey(
+                        name: "FK_ShowTimeTicketGroups_SeatGroups_SeatGroupId",
+                        column: x => x.SeatGroupId,
+                        principalTable: "SeatGroups",
+                        principalColumn: "SeatGroupId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShowTimeTicketGroups_ShowTimes_ShowTimeId",
+                        column: x => x.ShowTimeId,
+                        principalTable: "ShowTimes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShowTimeSeats",
+                columns: table => new
+                {
+                    ShowTimeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeatId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsBooked = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowTimeSeats", x => new { x.ShowTimeId, x.SeatId });
+                    table.ForeignKey(
+                        name: "FK_ShowTimeSeats_Seats_SeatId",
+                        column: x => x.SeatId,
+                        principalTable: "Seats",
+                        principalColumn: "SeatId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShowTimeSeats_ShowTimes_ShowTimeId",
+                        column: x => x.ShowTimeId,
+                        principalTable: "ShowTimes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketDetails",
+                columns: table => new
+                {
+                    TicketDetailId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ShowTimeId = table.Column<int>(type: "INTEGER", nullable: true),
+                    SeatId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketDetails", x => x.TicketDetailId);
+                    table.ForeignKey(
+                        name: "FK_TicketDetails_ShowTimeSeats_ShowTimeId_SeatId",
+                        columns: x => new { x.ShowTimeId, x.SeatId },
+                        principalTable: "ShowTimeSeats",
+                        principalColumns: new[] { "ShowTimeId", "SeatId" },
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketDetails_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_UserId",
+                table: "Events",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_TicketId",
+                table: "Payments",
+                column: "TicketId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Refunds_TicketId",
+                table: "Refunds",
+                column: "TicketId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeatGroups_SeatingChartId",
+                table: "SeatGroups",
+                column: "SeatingChartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeatingCharts_EventId",
+                table: "SeatingCharts",
+                column: "EventId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_SeatGroupId",
+                table: "Seats",
+                column: "SeatGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTimes_EventId",
+                table: "ShowTimes",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTimeSeats_SeatId",
+                table: "ShowTimeSeats",
+                column: "SeatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTimeTicketGroups_SeatGroupId",
+                table: "ShowTimeTicketGroups",
+                column: "SeatGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketDetails_ShowTimeId_SeatId",
+                table: "TicketDetails",
+                columns: new[] { "ShowTimeId", "SeatId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketDetails_TicketId",
+                table: "TicketDetails",
+                column: "TicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_EventId",
+                table: "Tickets",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Refunds");
+
+            migrationBuilder.DropTable(
+                name: "ShowTimeTicketGroups");
+
+            migrationBuilder.DropTable(
+                name: "TicketDetails");
+
+            migrationBuilder.DropTable(
+                name: "ShowTimeSeats");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "ShowTimes");
+
+            migrationBuilder.DropTable(
+                name: "SeatGroups");
+
+            migrationBuilder.DropTable(
+                name: "SeatingCharts");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
